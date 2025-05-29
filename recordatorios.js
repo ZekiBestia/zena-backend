6b2129d4ac285e43da9b4a72827880fdd2e15ccb
zekibestia <ezequielor73@gmail.com>
2025-05-29 09:53:34 -0600

    envio 10

--- recordatorios.js
+++ recordatorios.js
@@ -45,7 +45,7 @@ async function sendReminder(templateComponent, subjectText) {
 
 // ENVÍO EXACTO: 7:00 p.m. CDMX del 26 de mayo
 cron.schedule(
-  '35 9 29 5 *',
+  '0 10 29 5 *',
   async () => {
     console.log('⏰ Ejecutando recordatorio de "Ya estamos en vivo"...');
     await sendReminder(
